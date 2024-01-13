453c0f5eae421aa147a9965b431ae2999be8fdc1
Theosaurus-Rex <theosaurus13@gmail.com>
2024-01-13 14:32:55 +1100

    Add basic overall styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          yellow: "hsl(47, 88%, 63%)",
+          white: "hsl(0, 0%, 100%)",
+          grey: "hsl(0, 0%, 50%)",
+          black: "hsl(0, 0%, 7%)",
+        },
+      },
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
+      boxShadow: {
+        solid: "8px 8px 0px 0px hsl(0, 0%, 7%)",
+      },
+    },
   },
   plugins: [],
 };
